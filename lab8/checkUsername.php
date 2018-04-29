b2fba3f758b57b432fd67f9b83a332dcce507ab7
Judith Ramirez <judramirez@csumb.edu>
2018-04-29 04:05:44 +0000

    DB update

--- lab8/checkUsername.php
+++ lab8/checkUsername.php
@@ -6,7 +6,7 @@ $conn = getDatabaseConnection("heroku_154b829b8c54258");
 $username = $_GET['username'];
 //net query allows SQL Injection!
 //$sql = "SELECT * FROM lab8_user WHERE username = '$username' ";
-$sql = "SELECT * FROM lab8_user WHERE username = :username ";
+$sql = "SELECT * FROM lab8_user WHERE username = ':username' ";
 
 $stmt = $conn->prepare($sql);
 $stmt->execute( array(":username"=>$username) );
